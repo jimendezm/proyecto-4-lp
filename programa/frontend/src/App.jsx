af5d40e231d766d7a98fab76f658bfb9e88a0afd
jimendezm <132164589+jimendezm@users.noreply.github.com>
2025-06-21 17:41:11 -0600

    Update: Arreglo de los web sockets

--- programa/frontend/src/App.jsx
+++ programa/frontend/src/App.jsx
@@ -1,20 +1,13 @@
 // src/App.jsx
-import { useMemo } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { SocketContext } from './pages/SocketContext';
-import { io } from 'socket.io-client';
 import { Router } from './routes/router';
 
 function App() {
-  // Memoizar para no reconectar en cada render
-  const socket = useMemo(() => io('http://localhost:3001'), []);
 
   return (
-    <SocketContext.Provider value={socket}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </SocketContext.Provider>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
 
