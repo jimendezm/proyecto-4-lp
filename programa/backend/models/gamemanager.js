6b9f34d4c296bb4d8e039b4d20657a5e42c8c2e9
Ricardo Arce <yasserarce2404@gmail.com>
2025-06-26 08:36:06 -0600

    Missing untracked changes of previous commit

---
+++ programa/backend/models/gamemanager.js
@@ -0,0 +1,18 @@
+import Game from "./game.js";
+
+export default class GameManager {
+  static #instance;
+  games = []
+
+  constructor() {
+    if (GameManager.#instance) {
+      return GameManager.#instance;
+    }
+    GameManager.#instance = this;
+  }
+
+  addGame(roomName, id, type, track, numLaps, numPlayers) {
+    this.games.push(new Game(roomName, id, type, track, numLaps, numPlayers));
+    return this.games[this.games.length - 1]
+  }
+}
